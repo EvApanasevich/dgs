32ed122bc991efc9bfb1be1b93a897a01e74f6b6
Alexander Mamedov <smamedov451@gmail.com>
2023-11-24 13:58:22 +0300

    set container size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           {/* <header className="sticky top-0 z-[100]">
             <Navbar />
           </header> */}
-          <main className="container mx-auto px-5 min-h-screen flex-col items-center">
+          <main className="container mx-auto min-h-screen flex-col items-center">
             {/* <div className="flex">
               <Navigation />
             </div> */}
